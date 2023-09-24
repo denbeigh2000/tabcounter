ae3c24a20d735e540b06ae68b292eb21fb1ac197
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-23 18:54:50 -0700

    nix: sorted out relay nix building

--- flake.nix
+++ flake.nix
@@ -12,9 +12,13 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, systems, fenix }:
+  outputs = { self, nixpkgs, flake-utils, systems, fenix, naersk }:
     let
       allSystems = import systems;
     in
@@ -28,13 +32,27 @@
 
         inherit (pkgs.stdenvNoCC.hostPlatform) isDarwin isLinux;
 
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        toolchain = pkgs.fenix.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rustfmt"
+          "rustc"
+          "rust-src"
+        ];
+
         browseragent = pkgs.callPackage ./browseragent { };
+        relay = pkgs.callPackage ./relay { inherit toolchain; naersk = naersk'; };
 
         darwinPkgs = if isDarwin then [ pkgs.libiconv pkgs.darwin.Security ] else [ ];
         linuxPkgs = if isLinux then [ ] else [ ];  # TODO
       in
       {
         packages = rec {
+          inherit relay;
           agent = browseragent.builtDirectory;
           default = agent;
         };
@@ -49,14 +67,7 @@
               nodePackages.typescript
               nodePackages.rollup
 
-              (pkgs.fenix.complete.withComponents
-                [
-                  "cargo"
-                  "clippy"
-                  "rustfmt"
-                  "rustc"
-                  "rust-src"
-                ])
+              toolchain
               rust-analyzer-nightly
 
             ] ++ darwinPkgs ++ linuxPkgs;
