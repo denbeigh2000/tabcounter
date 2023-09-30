ea5d211d4cd56a3a12e9c56c3607efc356bcd637
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-30 12:55:02 -0700

    misc: bundle 3rd party deps (editor broken tho)
    in a weird situation with including 3rd party deps:
    - tsc requires module: nodenext to resolve node modules
    - rollup requires module: esnext to properly bundle deps
    - rollup will properly provide paths with node-resolve plugin, but this
      doesn't help editor plugins

--- browseragent/rollup.config.mjs
+++ browseragent/rollup.config.mjs
@@ -2,13 +2,13 @@
 // const typescript = require("@rollup/plugin-typescript");
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/background.ts",
   output: {
     sourcemap: false,
-    file: "build/background.js",
     format: "iife",
   },
-  plugins: [typescript(), terser()],
+  plugins: [nodeResolve(), typescript(), /* terser() */],
 };
