cccb71f850f1945dff276cd5d3b9ffd056861420
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-12-24 14:14:40 -0800

    browseragent: sort out dynamicism on options page

--- browseragent/src/entries/options/main.ts
+++ browseragent/src/entries/options/main.ts
@@ -1,46 +1,84 @@
-import { Message, Preferences, RequestPrefsMessage, SetPrefsMessage } from "@tabcounter/lib/events";
+import { Message, Preferences, RequestPrefsMessage, RequestStateMessage, SetPrefsMessage, State } from "@tabcounter/lib/events";
+
+
+const connected = {
+  str: "connected",
+  color: "green",
+};
+
+const disconnected = {
+  str: "not connected",
+  color: "red",
+};
+
+const show = (el: HTMLElement) => {
+  el.hidden = false;
+  el.style.display = "";
+}
+
+const hide = (el: HTMLElement) => {
+  el.hidden = true;
+  el.style.display = "none";
+}
 
 (async () => {
   const form = document.forms[0];
   const elements = {
-    port: form.elements.namedItem("port") as HTMLFormElement,
-    secret: form.elements.namedItem("secret") as HTMLFormElement,
+    loading: document.getElementById("loading-screen") as HTMLDivElement,
+    content: document.getElementById("content") as HTMLDivElement,
+
+    port: form.elements.namedItem("port") as HTMLInputElement,
+    secret: form.elements.namedItem("secret") as HTMLInputElement,
+
+    statusLabel: document.getElementById("connected-status") as HTMLSpanElement,
   };
-
-  console.debug("running stuff");
-
-  // const ready = new Promise<void>(resolve => {
-  //   browser.runtime.onInstalled.addListener(() => {
-  //     resolve();
-  //     console.debug("configuring options listeners");
-
-  //   });
-  // });
 
   console.debug("adding listener");
 
   browser.runtime.onMessage.addListener((message: Message) => {
     console.debug("message", message);
     if (message.type === "prefsUpdated") {
-      elements.port.value = message.data.port;
-      elements.secret.value = message.data.secret;
+      const { port, secret } = message.data;
+      elements.port.value = port.toString();
+      elements.secret.value = secret;
+
+      return;
+    }
+
+    if (message.type === "stateUpdated") {
+      const { str, color } = message.data.socketConnected ? connected : disconnected;
+      elements.statusLabel.innerText = str;
+      elements.statusLabel.style.color = color;
+      return;
     }
   });
 
   console.debug("added listener");
 
-  // await ready;
   const msg: RequestPrefsMessage = {
     type: "requestPrefs",
     data: null,
   };
 
   console.debug("sending prefs request");
-  let prefs = await browser.runtime.sendMessage(msg) as Preferences;
+  const prefs = await browser.runtime.sendMessage(msg) as Preferences;
   console.debug('prefs:', prefs);
+
+  const stateMsg: RequestStateMessage = {
+    type: "requestState",
+    data: null,
+  };
+  const state = await browser.runtime.sendMessage(stateMsg) as State;
 
   elements.port.value = prefs.port.toString();
   elements.secret.value = prefs.secret;
+
+  const { str, color } = state.socketConnected ? connected : disconnected;
+  elements.statusLabel.style.color = color;
+  elements.statusLabel.innerText = str;
+
+  hide(elements.loading);
+  show(elements.content);
 
   form.addEventListener("submit", (event: SubmitEvent) => {
     console.debug("running submit callback");
